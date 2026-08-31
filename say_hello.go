cff311489a3b05c8d4f3c44190cac6db735433ce
farisakbar28 <farisalvaro28@gmail.com>
2026-08-31 21:58:03 +0800

    update SayHello with address parameter

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,7 @@
 package go_say_hello
 
-func SayHello(name string) string {
-	return "Hello, mister " + name + "!"
+
+
+func SayHello(name string, address string) string {
+	return "Hello, mister " + name + " from " + address
 }
